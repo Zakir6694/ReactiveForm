f8ea51e5b1b258c7165af035101d599800effa3b
Zakir6694 <zakhussain45@gmail.com>
2019-09-24 11:54:38 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {FormGroup ,FormControl, Validators,FormBuilder,AbstractControl} from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  constructor(){}
-hide=true;
-onClickSubmit(value){
-   alert('Your Form has been submitted');
+export class AppComponent implements OnInit {
+  reactiveForm:FormGroup;
+  constructor(private fb:FormBuilder){}
+ ngOnInit(){
+   this.reactiveForm = this.fb.group({
+    emailId :["",[Validators.required]],
+    pass :["",[Validators.required]],
+    confirm :["",[Validators.required]]
+   });
+ }
+ erroMsg(field:string){
+   return !this.reactiveForm.get(field).valid && this.reactiveForm.get(field).touched ;
+ }
+ isFieldValid(formGroup: FormGroup) {
+  Object.keys(formGroup.controls).forEach(field => {
+    const control = formGroup.get(field);
+    if (control instanceof FormControl) {
+      control.markAsTouched({ onlySelf: true });
+    } else if (control instanceof FormGroup) {
+      this.isFieldValid(control);
+    }
+  });
 }
+
+reactiveSubmit(){
+  if(this.reactiveForm.valid){
+      alert('You have submitted the form successfully')
+  }
+  else{
+    this.isFieldValid(this.reactiveForm);
+  }
+}
+
+ /*errorMsg(field:string){
+   return this.reactiveForm.get(field).valid && this.reactiveForm.get(field).touched;
+ }*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {DemoMaterialModule} from './angular-material';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,DemoMaterialModule,BrowserAnimationsModule ],
+  imports:      [ BrowserModule, FormsModule,DemoMaterialModule,BrowserAnimationsModule,ReactiveFormsModule ],
   declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
